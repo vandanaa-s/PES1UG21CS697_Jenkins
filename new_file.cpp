cfe75add718a9048532b7e25f767efe52007e8ae
vandanaa-s <96181875+vandanaa-s@users.noreply.github.com>
2024-03-08 19:08:41 +0530

    Create new_file.cpp

---
+++ new_file.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Name: Vandana S\n";
+  cout << "SRN: PES1UG21CS697\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
